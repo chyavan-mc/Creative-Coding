692f0d89b9fd5e4d5c25ddc5e026a6ec8c25e365
Chyavan M C <chyavan.mc4@gmail.com>
2020-10-16 13:46:37 +0530

    For Github Pages

--- 12_links.js
+++ 12_links.js
@@ -1,88 +1,90 @@
 canvas = document.getElementById("links");
 context = canvas.getContext("2d");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-context.fillStyle="#38403C";
-context.fillRect(0,0,canvas.width,canvas.height);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.fillStyle = "#38403C";
+context.fillRect(0, 0, canvas.width, canvas.height);
 
-class Dot{
-    constructor(){
-        this.start = {x:canvas.width/2, y:canvas.height/2};
-        this.end = {x:(0.2+0.6*Math.random())*canvas.width, y:(0.2+0.6*Math.random())*canvas.height};
+class Dot {
+    constructor() {
+        this.start = { x: canvas.width / 2, y: canvas.height / 2 };
+        this.end = { x: (0.2 + 0.6 * Math.random()) * canvas.width, y: (0.2 + 0.6 * Math.random()) * canvas.height };
         this.now = this.start;
         this.now.prob = Math.random();
-        this.startR = (0.004+0.020*Math.random())*Math.min(canvas.width,canvas.height);
-        this.endR = (0.004+0.020*Math.random())*Math.min(canvas.width,canvas.height);
+        this.startR = (0.004 + 0.020 * Math.random()) * Math.min(canvas.width, canvas.height);
+        this.endR = (0.004 + 0.020 * Math.random()) * Math.min(canvas.width, canvas.height);
         this.nowR = this.startR;
-        this.dR = 0.01*(this.endR-this.startR);
-        this.D=euclid(this.start,this.end);
-        this.dr=0.01*this.D;
-        this.theta = Math.atan2(this.end.y-this.start.y, this.end.x-this.start.x);
+        this.dR = 0.01 * (this.endR - this.startR);
+        this.D = euclid(this.start, this.end);
+        this.dr = 0.01 * this.D;
+        this.theta = Math.atan2(this.end.y - this.start.y, this.end.x - this.start.x);
         this.count = 0;
     }
 
-    draw(){
+    draw() {
         context.beginPath();
-        context.arc(this.now.x,this.now.y,this.nowR,0,2*Math.PI);
+        context.arc(this.now.x, this.now.y, this.nowR, 0, 2 * Math.PI);
         context.fillStyle = "#E8F0E0";
         context.fill();
-        if(this.count<10){this.count++; return;}
+        if (this.count < 10) { this.count++; return; }
 
-        if(euclid(this.now,this.start)<euclid(this.start,this.end)/2){
-            this.dR+=0.0005*(this.endR-this.startR);
-            this.dr+=0.0005*this.D;}
-        else{
-            this.dR-=0.0005*(this.endR-this.startR);
-            this.dr-=0.0005*this.D;}
-        this.now.x += this.dr*Math.cos(this.theta);
-        this.now.y += this.dr*Math.sin(this.theta);
+        if (euclid(this.now, this.start) < euclid(this.start, this.end) / 2) {
+            this.dR += 0.0005 * (this.endR - this.startR);
+            this.dr += 0.0005 * this.D;
+        } else {
+            this.dR -= 0.0005 * (this.endR - this.startR);
+            this.dr -= 0.0005 * this.D;
+        }
+        this.now.x += this.dr * Math.cos(this.theta);
+        this.now.y += this.dr * Math.sin(this.theta);
         this.nowR += this.dR;
 
-        if((euclid(this.end,this.start)-euclid(this.start,this.now))/this.D < 0.01){
+        if ((euclid(this.end, this.start) - euclid(this.start, this.now)) / this.D < 0.01) {
             this.start.x = this.end.x;
             this.start.y = this.end.y;
-            this.end.x = (0.2+0.6*Math.random())*canvas.width;
-            this.end.y = (0.2+0.6*Math.random())*canvas.height;
+            this.end.x = (0.2 + 0.6 * Math.random()) * canvas.width;
+            this.end.y = (0.2 + 0.6 * Math.random()) * canvas.height;
             this.now.x = this.start.x;
             this.now.y = this.start.y;
             this.nowR = this.endR;
             this.startR = this.endR;
-            this.endR = (0.004+0.020*Math.random())*Math.min(canvas.width,canvas.height);
-            this.dR = 0.01*(this.endR-this.startR);
-            this.D=euclid(this.start,this.end);
-            this.dr=0.01*this.D;
-            this.theta = Math.atan2(this.end.y-this.start.y, this.end.x-this.start.x);
+            this.endR = (0.004 + 0.020 * Math.random()) * Math.min(canvas.width, canvas.height);
+            this.dR = 0.01 * (this.endR - this.startR);
+            this.D = euclid(this.start, this.end);
+            this.dr = 0.01 * this.D;
+            this.theta = Math.atan2(this.end.y - this.start.y, this.end.x - this.start.x);
             this.count = 0;
         }
     }
 }
 
-function euclid(objA,objB){
-    return Math.sqrt(((objA.x-objB.x)*(objA.x-objB.x))+((objA.y-objB.y)*(objA.y-objB.y)));
+function euclid(objA, objB) {
+    return Math.sqrt(((objA.x - objB.x) * (objA.x - objB.x)) + ((objA.y - objB.y) * (objA.y - objB.y)));
 }
 
-function lines(objA, objB){
+function lines(objA, objB) {
     context.beginPath();
     context.strokeStyle = "#E8F0E0";
-    context.moveTo(objA.now.x,objA.now.y);
-    context.lineTo(objB.now.x,objB.now.y);
-    context.lineWidth = (0.25*((objA.nowR + objB.nowR)/2)+0.1)*(Math.min(canvas.width,canvas.height)/800);
+    context.moveTo(objA.now.x, objA.now.y);
+    context.lineTo(objB.now.x, objB.now.y);
+    context.lineWidth = (0.25 * ((objA.nowR + objB.nowR) / 2) + 0.1) * (Math.min(canvas.width, canvas.height) / 800);
     context.lineCap = "round";
     context.stroke();
 }
 
 let a = new Array();
-const dots = 20;
-for(let i=0;i<dots;i++){
+const dots = 30;
+for (let i = 0; i < dots; i++) {
     a.push(new Dot());
 }
 
-setInterval(function(){
-    context.fillStyle="#38403C";
-    context.fillRect(0,0,canvas.width,canvas.height);
-    for(let i=0;i<dots-1;i++){
-        lines(a[i],a[i+1]);
+setInterval(function() {
+    context.fillStyle = "#38403C";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < dots - 1; i++) {
+        lines(a[i], a[i + 1]);
     }
-    for(let i=0;i<dots;i++){
-        a[i].draw();}
-    },25);
+    for (let i = 0; i < dots; i++) {
+        a[i].draw();
+    }
+}, 25);
--- 13_cubes.js
+++ 13_cubes.js
@@ -1,75 +1,75 @@
 const canvas = document.getElementById("cubes");
 const context = canvas.getContext("2d");
-canvas.width = 0.98*window.innerWidth;
-canvas.height = 0.98*window.innerHeight;
+canvas.width = 0.98 * window.innerWidth;
+canvas.height = 0.98 * window.innerHeight;
 context.fillStyle = "#202030";
-context.fillRect(0,0,canvas.width,canvas.height);
-const mid = {x:canvas.width/2, y:canvas.height/2};
+context.fillRect(0, 0, canvas.width, canvas.height);
+const mid = { x: canvas.width / 2, y: canvas.height / 2 };
 const numb = 12;
 
-class Cube{
-    constructor(i){
-        this.i=i;
-        this.a = (40+20*i)*(Math.min(mid.x,mid.y)/300)*(12/numb);
-        this.A = this.a/Math.sqrt(2);
-        this.B = this.a/2;
+class Cube {
+    constructor(i) {
+        this.i = i;
+        this.a = (40 + 20 * i) * (Math.min(mid.x, mid.y) / 300) * (12 / numb);
+        this.A = this.a / Math.sqrt(2);
+        this.B = this.a / 2;
         this.theta = 0;
         this.pt = new Array();
-        this.pt.push({y: mid.y - (this.a/2) + this.B*Math.sin(this.theta), x:mid.x + this.A*Math.cos(this.theta)});
-        this.pt.push({y: mid.y + (this.a/2) + this.B*Math.sin(this.theta), x:mid.x + this.A*Math.cos(this.theta)});
-        this.pt.push({y: mid.y - (this.a/2) - this.B*Math.sin(this.theta), x:mid.x - this.A*Math.cos(this.theta)});
-        this.pt.push({y: mid.y + (this.a/2) - this.B*Math.sin(this.theta), x:mid.x - this.A*Math.cos(this.theta)});
-        this.pt.push({y: mid.y - (this.a/2) + this.B*Math.sin(this.theta+Math.PI/2), x:mid.x + this.A*Math.cos(this.theta+Math.PI/2)});
-        this.pt.push({y: mid.y + (this.a/2) + this.B*Math.sin(this.theta+Math.PI/2), x:mid.x + this.A*Math.cos(this.theta+Math.PI/2)});
-        this.pt.push({y: mid.y - (this.a/2) - this.B*Math.sin(this.theta+Math.PI/2), x:mid.x - this.A*Math.cos(this.theta+Math.PI/2)});
-        this.pt.push({y: mid.y + (this.a/2) - this.B*Math.sin(this.theta+Math.PI/2), x:mid.x - this.A*Math.cos(this.theta+Math.PI/2)});
-        this.count= (70 - 7*i)*(numb/10);
+        this.pt.push({ y: mid.y - (this.a / 2) + this.B * Math.sin(this.theta), x: mid.x + this.A * Math.cos(this.theta) });
+        this.pt.push({ y: mid.y + (this.a / 2) + this.B * Math.sin(this.theta), x: mid.x + this.A * Math.cos(this.theta) });
+        this.pt.push({ y: mid.y - (this.a / 2) - this.B * Math.sin(this.theta), x: mid.x - this.A * Math.cos(this.theta) });
+        this.pt.push({ y: mid.y + (this.a / 2) - this.B * Math.sin(this.theta), x: mid.x - this.A * Math.cos(this.theta) });
+        this.pt.push({ y: mid.y - (this.a / 2) + this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x + this.A * Math.cos(this.theta + Math.PI / 2) });
+        this.pt.push({ y: mid.y + (this.a / 2) + this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x + this.A * Math.cos(this.theta + Math.PI / 2) });
+        this.pt.push({ y: mid.y - (this.a / 2) - this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x - this.A * Math.cos(this.theta + Math.PI / 2) });
+        this.pt.push({ y: mid.y + (this.a / 2) - this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x - this.A * Math.cos(this.theta + Math.PI / 2) });
+        this.count = (70 - 7 * i) * (numb / 10);
     }
 
-    drawLine(pt1,pt2, width){
+    drawLine(pt1, pt2, width) {
         context.beginPath();
-        context.strokeStyle = "rgba(255,255,255,"+ (1-(this.i/(1.2*numb)))+")";
-        context.moveTo(pt1.x,pt1.y);
-        context.lineTo(pt2.x,pt2.y);
+        context.strokeStyle = "rgba(255,255,255," + (1 - (this.i / (1.2 * numb))) + ")";
+        context.moveTo(pt1.x, pt1.y);
+        context.lineTo(pt2.x, pt2.y);
         context.lineWidth = width;
         context.lineCap = "round";
         context.stroke();
     }
 
-    show(){
-        for(let i=0; i<4;i++){
-            this.drawLine(this.pt[2*i],this.pt[2*i+1],1);
+    show() {
+        for (let i = 0; i < 4; i++) {
+            this.drawLine(this.pt[2 * i], this.pt[2 * i + 1], 1);
 
-            this.drawLine(this.pt[i],this.pt[4+(i%2)],1);
-            this.drawLine(this.pt[i],this.pt[6+(i%2)],1);
+            this.drawLine(this.pt[i], this.pt[4 + (i % 2)], 1);
+            this.drawLine(this.pt[i], this.pt[6 + (i % 2)], 1);
         }
-        if((this.pt[0].x == (mid.x + this.A) || this.pt[0].x == (mid.x - this.A)) && this.count < 40+6*numb){
-            this.count+=1;
+        if ((this.pt[0].x == (mid.x + this.A) || this.pt[0].x == (mid.x - this.A)) && this.count < 40 + 6 * numb) {
+            this.count += 1;
             return;
         }
 
-        this.theta-=0.025*Math.PI;
-        this.count=0;
-        this.pt[0] = {y: mid.y - (this.a/2) + this.B*Math.sin(this.theta), x:mid.x + this.A*Math.cos(this.theta)};
-        this.pt[1] = {y: mid.y + (this.a/2) + this.B*Math.sin(this.theta), x:mid.x + this.A*Math.cos(this.theta)};
-        this.pt[2] = {y: mid.y - (this.a/2) - this.B*Math.sin(this.theta), x:mid.x - this.A*Math.cos(this.theta)};
-        this.pt[3] = {y: mid.y + (this.a/2) - this.B*Math.sin(this.theta), x:mid.x - this.A*Math.cos(this.theta)};
-        this.pt[4] = {y: mid.y - (this.a/2) + this.B*Math.sin(this.theta+Math.PI/2), x:mid.x + this.A*Math.cos(this.theta+Math.PI/2)};
-        this.pt[5] = {y: mid.y + (this.a/2) + this.B*Math.sin(this.theta+Math.PI/2), x:mid.x + this.A*Math.cos(this.theta+Math.PI/2)};
-        this.pt[6] = {y: mid.y - (this.a/2) - this.B*Math.sin(this.theta+Math.PI/2), x:mid.x - this.A*Math.cos(this.theta+Math.PI/2)};
-        this.pt[7] = {y: mid.y + (this.a/2) - this.B*Math.sin(this.theta+Math.PI/2), x:mid.x - this.A*Math.cos(this.theta+Math.PI/2)};
+        this.theta -= 0.005 * Math.PI;
+        this.count = 0;
+        this.pt[0] = { y: mid.y - (this.a / 2) + this.B * Math.sin(this.theta), x: mid.x + this.A * Math.cos(this.theta) };
+        this.pt[1] = { y: mid.y + (this.a / 2) + this.B * Math.sin(this.theta), x: mid.x + this.A * Math.cos(this.theta) };
+        this.pt[2] = { y: mid.y - (this.a / 2) - this.B * Math.sin(this.theta), x: mid.x - this.A * Math.cos(this.theta) };
+        this.pt[3] = { y: mid.y + (this.a / 2) - this.B * Math.sin(this.theta), x: mid.x - this.A * Math.cos(this.theta) };
+        this.pt[4] = { y: mid.y - (this.a / 2) + this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x + this.A * Math.cos(this.theta + Math.PI / 2) };
+        this.pt[5] = { y: mid.y + (this.a / 2) + this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x + this.A * Math.cos(this.theta + Math.PI / 2) };
+        this.pt[6] = { y: mid.y - (this.a / 2) - this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x - this.A * Math.cos(this.theta + Math.PI / 2) };
+        this.pt[7] = { y: mid.y + (this.a / 2) - this.B * Math.sin(this.theta + Math.PI / 2), x: mid.x - this.A * Math.cos(this.theta + Math.PI / 2) };
 
     }
 }
 
 let a = new Array();
-for(let i=0; i<numb; i++){
+for (let i = 0; i < numb; i++) {
     a.push(new Cube(i));
 }
-setInterval(function(){
+setInterval(function() {
     context.fillStyle = "#202030";
-    context.fillRect(0,0,canvas.width,canvas.height);
-    for(let i=0; i<numb; i++){
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < numb; i++) {
         a[i].show();
     }
-}, 25);
+}, 10);
